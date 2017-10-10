379ca7925ef98b73020c3fd02100623e8bab0f9d
mmolesworth <m.molesworth@gmail.com>
2017-10-10 16:13:02 -0400

    Add files via upload

---
+++ flask-scaffolding.sh
@@ -0,0 +1,26 @@
+#
+# Create scaffolding for a flask project
+# Based upon the lesson found on acloud.guru - Docker for DevOps
+# https://acloud.guru/course/docker-devops-from-development-to-production/learn/dockerized-flask-app/scaffolding-project/watch
+#
+#
+
+PROJECT-NAME=$1
+
+mkdir -p ~/Projects/$PROJECT-NAME/${$PROJECT-NAME,,}
+
+cd ~/Projects/$PROJECT-NAME/${$PROJECT-NAME,,}
+
+touch requirements.txt .gitignore Dockerfile docker-compose.yml .dockerignore
+mkdir ${$PROJECT-NAME,,} config instance
+
+touch config/__init__.py config/settings.py instance/__init__.py
+instance/settings.py_production_example
+
+cd ${$PROJECT-NAME,,}
+
+touch app.py __init__.py
+
+mkdir templates static
+
+touch templates/layout.html static/main.css
